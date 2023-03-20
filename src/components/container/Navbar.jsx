ecc4b3be53ad363b7a30ad115e8a9e0965674ebc
Sujit Kumar <mnamesujit@gmail.com>
2023-03-21 00:12:33 +0530

    Navigation Menu done

--- src/components/container/Navbar.jsx
+++ src/components/container/Navbar.jsx
@@ -1,7 +1,59 @@
-const Navbar = () => {
-  return <div id="Navbar">
-    <h1>Navigation bar</h1>
-  </div>;
-};
+import { BiHomeHeart } from "react-icons/bi";
+import { ImRocket } from "react-icons/im";
+import { GiArrowScope, GiTentacleStrike } from "react-icons/gi";
+import { MdOutlineContactless } from "react-icons/md";
 
-export default Navbar;
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div id="Navbar">
+      <div className="items">
+        <div className="tooltip">Home</div>
+        <div className="icon">
+          <a href="#Home">
+            <BiHomeHeart />
+          </a>
+        </div>
+      </div>
+
+      <div className="items">
+        <div className="tooltip">About</div>
+        <div className="icon">
+          <a href="#About">
+            <GiTentacleStrike />
+          </a>
+        </div>
+      </div>
+
+      <div className="items">
+        <div className="tooltip">Skills</div>
+        <div className="icon">
+          <a href="#Skills">
+            <GiArrowScope />
+          </a>
+        </div>
+      </div>
+
+      <div className="items">
+        <div className="tooltip">Work</div>
+        <div className="icon">
+          <a href="#Work">
+            <ImRocket />
+          </a>
+        </div>
+      </div>
+
+      <div className="items">
+        <div className="tooltip">Contact</div>
+        <div className="icon">
+          <a href="#Contact">
+            <MdOutlineContactless />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
